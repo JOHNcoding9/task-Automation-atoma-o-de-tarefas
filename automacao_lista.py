87c1cb90bafbc1d963b5ace4ea1f5dec78031d7e
João Vitor de Oliveira Lima <160860021+JOHNcoding9@users.noreply.github.com>
2024-07-09 22:16:43 -0300

    Update automacao_lista.py

--- automacao_lista.py
+++ automacao_lista.py
@@ -3,50 +3,55 @@
 #3 etapa : fazer o login
 #4 etapa: cadastrar o produto
 #5 etapa repetir esse processo até o fim da lista
-import pyautogui
-import pandas
+import pyautogui #o pyautogui é uma ferramenta de automação de tarefas 
+import pandas #o pandas é uma ferramente de manipulação e análise de dados
 tabela=pandas.read_csv("produtos.csv")
 from time import sleep 
 pyautogui.PAUSE= 0.5
-pyautogui.press("win")
+#entrando no chrome
+yautogui.press("win")
 pyautogui.write("Chrome")
 pyautogui.press("enter")
+#entrando no link do site da loja de produtos
 pyautogui.click(x=540, y=852)
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
+#intervalo para esperar o site carregar
 sleep(2)
+#inserção das credencias do funcionário
 pyautogui.click(x=682, y=375)
 pyautogui.write("JOHNlindo2024@gmail.com")
 pyautogui.press("tab")
 pyautogui.write("7894361211")
 pyautogui.click(x=948, y=530)
+#aguardando o site recarregar
 sleep(3)
-#código do produto
+#inserir código do produto
 for linha in tabela.index:
  pyautogui.click(x=680, y=255)
  codigo=str(tabela.loc[linha,"codigo"]) #localizar na tabela a linha (1 por exemplo) na coluna ("codigo")
  pyautogui.write(codigo)
-#Marca
+#inserir a Marca
  pyautogui.press("tab")
  marca=str(tabela.loc[linha,"marca"])
  pyautogui.write(marca)
-#tipo
+#inserir o tipo
  pyautogui.press("tab")
  tipo=str(tabela.loc[linha,"tipo"])
  pyautogui.write(tipo)
-#categoria
+#inserir a categoria
  pyautogui.press("tab")
  categoria=str(tabela.loc[linha,"categoria"])
  pyautogui.write(categoria)
-#preço
+#inserir o preço
  pyautogui.press("tab")
  preco_unitario=str(tabela.loc[linha,"preco_unitario"])
  pyautogui.write(preco_unitario)
-#custo
+#inserir o custo
  pyautogui.press("tab")
  custo=str(tabela.loc[linha,"custo"])
  pyautogui.write(custo)
-#obs
+#observações
  pyautogui.press("tab")
  obs=str(tabela.loc[linha,"obs"])
  if obs!="nan":
